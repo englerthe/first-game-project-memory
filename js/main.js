4fd4008ed872dbfeccb9aca7cf8c9264b7288ab9
Englert <englert@media-saturn.com>
2019-09-04 10:14:21 +0200

    inital commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const cards = [
+    { name: 'aquaman',         img: 'aquaman.jpg' },
+    { name: 'batman',          img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+    { name: 'flash',           img: 'flash.jpg' },
+    { name: 'green arrow',     img: 'green-arrow.jpg' },
+    { name: 'green lantern',   img: 'green-lantern.jpg' },
+    { name: 'ironman',         img: 'ironman.jpg' },
+    { name: 'spiderman',       img: 'spiderman.jpg' },
+    { name: 'superman',        img: 'superman.jpg' },
+    { name: 'the avengers',    img: 'the-avengers.jpg' },
+    { name: 'thor',            img: 'thor.jpg' },
+    { name: 'aquaman',         img: 'aquaman.jpg' },
+    { name: 'batman',          img: 'batman.jpg' },
+    { name: 'captain america', img: 'captain-america.jpg' },
+    { name: 'fantastic four',  img: 'fantastic-four.jpg' },
+    { name: 'flash',           img: 'flash.jpg' },
+    { name: 'green arrow',     img: 'green-arrow.jpg' },
+    { name: 'green lantern',   img: 'green-lantern.jpg' },
+    { name: 'ironman',         img: 'ironman.jpg' },
+    { name: 'spiderman',       img: 'spiderman.jpg' },
+    { name: 'superman',        img: 'superman.jpg' },
+    { name: 'the avengers',    img: 'the-avengers.jpg' },
+    { name: 'thor',            img: 'thor.jpg' }
+  ];
+
+let memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let html = '';
+    html += `<div class="card" data-card-name="${pic.name}">`;
+    html += `<div class="back" name="${pic.img}"></div>`;
+    html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+    html += `</div>`;
+    });
+// load cards and start game
+//document.addEventListener("DOMContentLoaded", function(event) { 
+    let html = '';
+    memoryGame.cards.forEach(pic => {
+      html += `<div class="card" data-card-name="${pic.name}">`;
+      html += `<div class="back" name="${pic.img}"></div>`;
+      html += `<div class="front" style="background: url(img/${pic.img}) no-repeat"></div>`;
+      html += `</div>`;
+    });
+  
+    // Add all the divs to the HTML
+    document.querySelector('#memory_board').innerHTML = html;
+  
+    // Bind the click event of each element to a function
+    document.querySelectorAll('.back').forEach( card => {
+      card.onclick = function() {
+        // TODO: write some code here
+        memoryGame.cardClicked(card);
+   
+          // memoryGame.checkIfPair (card[1]; card[2])
+      };
+    });
+  })
---
+++ js/players.js
@@ -0,0 +1,8 @@
+class players {
+    constructor(nameInput){
+        this.name = nameInput;
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
+}
