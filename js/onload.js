91685e4db6321120b572f4c73ee00b3860e9e6b4
Englert <englert@media-saturn.com>
2019-09-17 09:36:01 +0200

    js_compatible version with babeljs

--- js/onload.js
+++ js/onload.js
@@ -1,79 +1,90 @@
+"use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-//create HTML
+document.addEventListener("DOMContentLoaded", function () {
+  //create HTML
+  var initialhtml = '';
+  initialhtml += "<label>Bitte Anzahl der Spieler w\xE4hlen</label>";
+  initialhtml += "<div id=\"start_game\">"; // begin start_game
 
-let initialhtml = '';
-initialhtml += `<label>Bitte Anzahl der Spieler wählen</label>`;
-initialhtml += `<div id="start_game">`; // begin start_game
-initialhtml += `<label><ul><li><input type="radio" name="Players" checked value="1">1 Spieler</li></ul></label>`;
-initialhtml += `<label><ul><li><input type="radio" name="Players" value="2">2 Spieler</li></ul></label>`;
-initialhtml += `<label><ul><li><input type="radio" name="Players" value="3">3 Spieler</li></ul></label>`;
-initialhtml += `<label><ul><li><input type="radio" name="Players" value="4">4 Spieler</li></ul></label>`;
-initialhtml += `</div>`; // end start_game
-initialhtml += `<button onclick="howManyPlayers()">Weiter geht´s, Namen eingeben</button>`;
-initialhtml += `<div id="regeln">`;
-initialhtml += `<h2>Regeln:<br>Das Spiel besteht aus 24 Feldern mit 12 gleichen Bildern. Ziel ist es möglichst viele Paare zu finden.
-Ein Spieler deckt zwei Felder auf, sind die Bilder identisch hat er ein Paar und darf noch einmal aufdecken.
-Sind die Bilder nicht gleich, ist der nächste Spieler dran.<br>Viel Spass!</h2>`;
-initialhtml += `</div>`;
-initialhtml += `</container>`; // end main_game
+  initialhtml += "<label><ul><li><input type=\"radio\" name=\"Players\" checked value=\"1\">1 Spieler</li></ul></label>";
+  initialhtml += "<label><ul><li><input type=\"radio\" name=\"Players\" value=\"2\">2 Spieler</li></ul></label>";
+  initialhtml += "<label><ul><li><input type=\"radio\" name=\"Players\" value=\"3\">3 Spieler</li></ul></label>";
+  initialhtml += "<label><ul><li><input type=\"radio\" name=\"Players\" value=\"4\">4 Spieler</li></ul></label>";
+  initialhtml += "</div>"; // end start_game
 
-// Add all the divs to the HTML
-document.querySelector('#initial_load').innerHTML = initialhtml;
+  initialhtml += "<button onclick=\"howManyPlayers()\">Weiter geht\xB4s, Namen eingeben</button>";
+  initialhtml += "<div id=\"regeln\">";
+  initialhtml += "<h2>Regeln:<br>Das Spiel besteht aus 24 Feldern mit 12 gleichen Bildern. Ziel ist es m\xF6glichst viele Paare zu finden.\nEin Spieler deckt zwei Felder auf, sind die Bilder identisch hat er ein Paar und darf noch einmal aufdecken.\nSind die Bilder nicht gleich, ist der n\xE4chste Spieler dran.<br>Viel Spass!</h2>";
+  initialhtml += "</div>";
+  initialhtml += "</container>"; // end main_game
+  // Add all the divs to the HTML
 
-// end creation of HTML
-});
-//first game function
-let choosePlayerNumber = ``;
-function howManyPlayers(){
-    var checkboxes = document.querySelectorAll('input');
-    for (let i=0; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked === true) {
-            choosePlayerNumber = parseInt(checkboxes[i].value);
-        }
-    } 
-    let choosePlayerhtml =`<div id="start_game">`;
-    choosePlayerhtml +=`<form>`;
-    choosePlayerhtml +=`<label form="selectedplayers">Bitte Spielername eingeben (max. 20 Zeichen)</label><br><br>`;
-    for (let j = 1; j <= parseInt(choosePlayerNumber); j++) {
-        choosePlayerhtml +=`<label>Spieler ${j}</label>`;
-        choosePlayerhtml +=`<input type="text" id="player_${j}" maxlength="20" required/><br>`;
+  document.querySelector('#initial_load').innerHTML = initialhtml; // end creation of HTML
+}); //first game function
+
+var choosePlayerNumber = "";
+
+function howManyPlayers() {
+  var checkboxes = document.querySelectorAll('input');
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      choosePlayerNumber = parseInt(checkboxes[i].value);
     }
-    choosePlayerhtml +=`<br><button onclick="writePlayersAndStartGame()">Spiel starten</button>`;
-    choosePlayerhtml +=`</form>`;
-    choosePlayerhtml +=`</div>`;
-    document.querySelector('#initial_load').innerHTML = choosePlayerhtml;
-}
-// read playernames from input and handover to game
-let playersNames = [];
+  }
+
+  var choosePlayerhtml = "<div id=\"start_game\">";
+  choosePlayerhtml += "<form>";
+  choosePlayerhtml += "<label form=\"selectedplayers\">Bitte Spielername eingeben (max. 20 Zeichen)</label><br><br>";
+
+  for (var j = 1; j <= parseInt(choosePlayerNumber); j++) {
+    choosePlayerhtml += "<label>Spieler ".concat(j, "</label>");
+    choosePlayerhtml += "<input type=\"text\" id=\"player_".concat(j, "\" maxlength=\"20\" required/><br>");
+  }
+
+  choosePlayerhtml += "<br><button onclick=\"writePlayersAndStartGame()\">Spiel starten</button>";
+  choosePlayerhtml += "</form>";
+  choosePlayerhtml += "</div>";
+  document.querySelector('#initial_load').innerHTML = choosePlayerhtml;
+} // read playernames from input and handover to game
+
+
+var playersNames = [];
+
 function writePlayersAndStartGame() {
-    for (let k=1; k <= parseInt(choosePlayerNumber); k++) {
-        if (document.getElementById(`player_${k}`).value == ``){
-            playersNames.push(`Spieler ${k}`);
-        } else {
-            playersNames.push(document.getElementById(`player_${k}`).value);
-        }
-        //playersNames.push(document.getElementById(`player_${k}`).value);
-    }
-    startGame(playersNames); //start maingame 
+  for (var k = 1; k <= parseInt(choosePlayerNumber); k++) {
+    if (document.getElementById("player_".concat(k)).value == "") {
+      playersNames.push("Spieler ".concat(k));
+    } else {
+      playersNames.push(document.getElementById("player_".concat(k)).value);
+    } //playersNames.push(document.getElementById(`player_${k}`).value);
+
+  }
+
+  startGame(playersNames); //start maingame 
+} //popup window for next players
+
+
+function createPopupObjectFromID(id) {
+  //handover "mypopup" id
+  return document.getElementById(id);
 }
 
-//popup window for next players
-function createPopupObjectFromID(id) { //handover "mypopup" id
-    return document.getElementById(id);
-} 
-
-let orgPlayerText = "";
-let newPlayerText ="";
+var orgPlayerText = "";
+var newPlayerText = "";
 
 function showPopup(id, name) {
-    orgPlayerText = document.getElementById("playertext"); //read string from popup html
-    newPlayerText = document.createTextNode(`Nächster Spieler: ${name}`); //create new string with playername
-    orgPlayerText.appendChild(newPlayerText); //create new text
-    createPopupObjectFromID(id).style.display ='block'; // show popup and block background(game)
-    
+  orgPlayerText = document.getElementById("playertext"); //read string from popup html
+
+  newPlayerText = document.createTextNode("N\xE4chster Spieler: ".concat(name)); //create new string with playername
+
+  orgPlayerText.appendChild(newPlayerText); //create new text
+
+  createPopupObjectFromID(id).style.display = 'block'; // show popup and block background(game)
 }
+
 function hidePopup(id) {
-    createPopupObjectFromID(id).style.display ='none'; // hide popup and reactivate background(game)
-    orgPlayerText.removeChild(newPlayerText); //reset string in popup
+  createPopupObjectFromID(id).style.display = 'none'; // hide popup and reactivate background(game)
+
+  orgPlayerText.removeChild(newPlayerText); //reset string in popup
 }
--- js/players.js
+++ js/players.js
@@ -1,76 +1,117 @@
-class players {
-    constructor(nameInput){
-        this.name = nameInput;
-        this.pickedCards = [];
-        this.pairsClicked = 0;
-        this.pairsGuessed = 0;
-        this.game = null;
-    }
-    // game players logic
-    // check if cards are pairs
-    checkIfPair(card1, card2) {
-        this.pairsClicked +=1;
-        document.getElementById(`pairs_clicked_${this.name}`).innerHTML = this.pairsClicked;
-        this.pickedCards = [card1, card2];
-        if (this.pickedCards[0].getAttribute("name") === this.pickedCards[1].getAttribute("name")) {
-          this.pairsGuessed +=1;
-          this.game.countPairsGuessed += 1;
-          //this.game.countPlayersPairsGuessed.push(this.name); //schreibe Spielername in array - workaround
-          document.getElementById(`pairs_guessed_${this.name}`).innerHTML = this.pairsGuessed;
-          return true;
-        }
-        return false;
-    }
-    // check how many cards clicked by player
-    cardClicked(card) {
-        if (this.pickedCards.length == 2) {
-          return;
-        }
-        card.className = "front";
-        card.parentNode.childNodes[1].className = "back";
-        this.pickedCards.push(card);
-        if (this.pickedCards.length == 2) {
-          if (this.checkIfPair(this.pickedCards[0], this.pickedCards[1]) == false ) { //check if selected cards are not equal  
-            this.flipCardsBack();
-          } else { // if cards are equal (a pair)
-            this.isFinished();
-            this.pickedCards = []; 
-          }
-        }
-    }
-    // check if player has won
-    isFinished() {
-        if (this.pairsClicked < 1) { //sind 2 Karten aufgedeckt worden?
-          return false;
-        } else if (this.game.countPairsGuessed < this.game.cards.length/2) { // sind schon alle Karten offen?
-          return false;
-        }
-        let winner = "";
-        for (let i=0; i < (this.game.players.length - 1) ;i++) { //fuehre fuer jeden Spieler die Pruefung aus (letzte muss nicht)
-          if (this.game.players[i].pairsGuessed > this.game.players[i+1].pairsGuessed) { //habe ich mehr Paare gefunden als der naechste? 
-            winner = this.game.players[i].name;
-          }
-          else if (this.game.players[i].pairsGuessed == this.game.players[i+1].pairsGuessed && this.game.players[i].pairsClicked < this.game.players[i+1].pairsClicked) { //wenn ich gleichviel Paare habe aber weniger Versuche ... 
-              winner = this.game.players[i].name;
-          }
-          else {
-            winner = this.game.players[i+1].name; // naechster Spieler hat mehr Paare gefunden oder gleiche Anzahl Paare und Versuche
-          }
-        }
-        //let winner = this.game.countPlayersPairsGuessed.sort(); //sortiere array (meiste elemente sind vorne) nur ein workaround!
-        //document.querySelector('#finished').innerHTML = `<p>Spieler<br> ${winner[0]} <br>hat gewonnen !</p><button onclick="location.reload()">Neues Spiel</button>`;
-        document.querySelector('#finished').innerHTML = `<p>Spieler<br> ${winner} <br>hat gewonnen !</p><button onclick="location.reload()">Neues Spiel</button>`;
+"use strict";
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var players =
+/*#__PURE__*/
+function () {
+  function players(nameInput) {
+    _classCallCheck(this, players);
+
+    this.name = nameInput;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+    this.game = null;
+  } // game players logic
+  // check if cards are pairs
+
+
+  _createClass(players, [{
+    key: "checkIfPair",
+    value: function checkIfPair(card1, card2) {
+      this.pairsClicked += 1;
+      document.getElementById("pairs_clicked_".concat(this.name)).innerHTML = this.pairsClicked;
+      this.pickedCards = [card1, card2];
+
+      if (this.pickedCards[0].getAttribute("name") === this.pickedCards[1].getAttribute("name")) {
+        this.pairsGuessed += 1;
+        this.game.countPairsGuessed += 1; //this.game.countPlayersPairsGuessed.push(this.name); //schreibe Spielername in array - workaround
+
+        document.getElementById("pairs_guessed_".concat(this.name)).innerHTML = this.pairsGuessed;
+        return true;
       }
-    // in case of no pairs, flip cards back after timeout - to memorize cards :-)
-    flipCardsBack() {
-        setTimeout( () => {
-            this.pickedCards[0].className = "back";
-            this.pickedCards[0].parentNode.childNodes[1].className = "front";
-            this.pickedCards[1].className = "back";
-            this.pickedCards[1].parentNode.childNodes[1].className = "front";
-            this.pickedCards = [];
-            this.game.switchPlayer(); // initiate to change player // flip selected cards back
-        }, 800);
+
+      return false;
+    } // check how many cards clicked by player
+
+  }, {
+    key: "cardClicked",
+    value: function cardClicked(card) {
+      if (this.pickedCards.length == 2) {
+        return;
+      }
+
+      card.className = "front";
+      card.parentNode.childNodes[1].className = "back";
+      this.pickedCards.push(card);
+
+      if (this.pickedCards.length == 2) {
+        if (this.checkIfPair(this.pickedCards[0], this.pickedCards[1]) == false) {
+          //check if selected cards are not equal  
+          this.flipCardsBack();
+        } else {
+          // if cards are equal (a pair)
+          this.isFinished();
+          this.pickedCards = [];
+        }
+      }
+    } // check if player has won
+
+  }, {
+    key: "isFinished",
+    value: function isFinished() {
+      if (this.pairsClicked < 1) {
+        //sind 2 Karten aufgedeckt worden?
+        return false;
+      } else if (this.game.countPairsGuessed < this.game.cards.length / 2) {
+        // sind schon alle Karten offen?
+        return false;
+      }
+
+      var winner = "";
+
+      for (var i = 0; i < this.game.players.length - 1; i++) {
+        //fuehre fuer jeden Spieler die Pruefung aus (letzte muss nicht)
+        if (this.game.players[i].pairsGuessed > this.game.players[i + 1].pairsGuessed) {
+          //habe ich mehr Paare gefunden als der naechste? 
+          winner = this.game.players[i].name;
+        } else if (this.game.players[i].pairsGuessed == this.game.players[i + 1].pairsGuessed && this.game.players[i].pairsClicked < this.game.players[i + 1].pairsClicked) {
+          //wenn ich gleichviel Paare habe aber weniger Versuche ... 
+          winner = this.game.players[i].name;
+        } else {
+          winner = this.game.players[i + 1].name; // naechster Spieler hat mehr Paare gefunden oder gleiche Anzahl Paare und Versuche
+        }
+      } //let winner = this.game.countPlayersPairsGuessed.sort(); //sortiere array (meiste elemente sind vorne) nur ein workaround!
+      //document.querySelector('#finished').innerHTML = `<p>Spieler<br> ${winner[0]} <br>hat gewonnen !</p><button onclick="location.reload()">Neues Spiel</button>`;
+
+
+      document.querySelector('#finished').innerHTML = "<p>Spieler<br> ".concat(winner, " <br>hat gewonnen !</p><button onclick=\"location.reload()\">Neues Spiel</button>");
+    } // in case of no pairs, flip cards back after timeout - to memorize cards :-)
+
+  }, {
+    key: "flipCardsBack",
+    value: function flipCardsBack() {
+      var _this = this;
+
+      setTimeout(function () {
+        _this.pickedCards[0].className = "back";
+        _this.pickedCards[0].parentNode.childNodes[1].className = "front";
+        _this.pickedCards[1].className = "back";
+        _this.pickedCards[1].parentNode.childNodes[1].className = "front";
+        _this.pickedCards = [];
+
+        _this.game.switchPlayer(); // initiate to change player // flip selected cards back
+
+      }, 800);
     }
-    
-}
+  }]);
+
+  return players;
+}();
